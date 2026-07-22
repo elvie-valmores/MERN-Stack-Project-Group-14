add4e9116e28deb242ec6592c9ce1c9d953937cd
Moaaz Hany <286407445+Moaaz159@users.noreply.github.com>
2026-07-22 18:09:23 -0400

    Load only Latin Poppins fonts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/500.css";
-import "@fontsource/poppins/600.css";
-import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/latin-400.css";
+import "@fontsource/poppins/latin-500.css";
+import "@fontsource/poppins/latin-600.css";
+import "@fontsource/poppins/latin-700.css";
 
 import App from "./App.jsx";
 
